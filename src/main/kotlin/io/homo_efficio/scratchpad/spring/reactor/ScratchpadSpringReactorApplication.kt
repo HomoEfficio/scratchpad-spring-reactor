6c8231b79a30498f1393d65fd9e9de8f75a2c53c
Homo Efficio <homo.efficio@gmail.com>
2022-07-03 11:48:48 +0900

    Initial Commit

---
+++ src/main/kotlin/io/homo_efficio/scratchpad/spring/reactor/ScratchpadSpringReactorApplication.kt
@@ -0,0 +1,11 @@
+package io.homo_efficio.scratchpad.spring.reactor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScratchpadSpringReactorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScratchpadSpringReactorApplication>(*args)
+}
---
+++ src/test/kotlin/io/homo_efficio/scratchpad/spring/reactor/ScratchpadSpringReactorApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.homo_efficio.scratchpad.spring.reactor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScratchpadSpringReactorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
