20596ce90063a931ce25ae2ee00ba52db9ad5df2
Homo Efficio <homo.efficio@gmail.com>
2022-07-10 16:35:41 +0900

    CacheConfig 추가

---
+++ src/main/kotlin/io/homo_efficio/scratchpad/spring/reactor/config/CacheConfig.kt
@@ -0,0 +1,9 @@
+package io.homo_efficio.scratchpad.spring.reactor.config
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@EnableCaching
+@Configuration
+class CacheConfig {
+}
